84ef73a9df91199ec8934401089c9bea57b9e0de
HengMrZ <810216626@qq.com>
2018-11-01 22:12:49 +0800

    Create ipinwangduan.py
    判断ip是不是在网段范围

---
+++ ipinwangduan.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+import sys
+from IPy import IP
+
+try:
+    f = open('./iplist', 'r')
+    for line in f.readlines():
+      ipadd=line.strip()
+      try:
+         g = open('./wangduan', 'r')
+         for ip in g.readlines():
+           ip1=ip.strip()
+           if  ipadd in IP(ip1):
+             print ipadd
+             break
+           else:
+             continue
+      finally:
+          if g:
+            g.close()
+finally:
+    if f:
+        f.close()
